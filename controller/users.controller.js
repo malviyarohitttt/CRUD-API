6447cd52e21d0a1701b44807fee26fe1c3dad87a
Rohit Malviya <rohitmalviya2324@gmail.com>
2024-12-21 13:21:15 +0530

    Controller Added

--- controller/users.controller.js
+++ controller/users.controller.js
@@ -13,6 +13,45 @@ class userController {
             callback({ status: false, error: error.message });
           }
     }
+    async addUser(newUser,callback){
+          try {
+            let isAdded = await connection.collection("Users").insertOne(newUser);
+            if (isAdded) {
+              callback({ status: true, msg: "New User Added" });
+            } else {
+              callback({ status: false, msg: "Failed to Add New User" });
+            }
+          } catch (error) {
+            callback({ status: false, error: error.message });
+          }
+    }
+    async deleteUser(id,callback){
+        try {
+            let isDeleted = await connection.collection("Users").deleteOne({ _id: new ObjectId(id)});
+            if (isDeleted) {
+              callback({ status: true, msg: "User Deleted" });
+            } else {
+              callback({ status: false, msg: "Failed to Delete User" });
+            }
+          } catch (error) {
+            callback({ status: false, error: error.message });
+          }
+    }
+    async editUser(data,callback){
+        try {
+            isUpdated = await connection.collection("Users").updateOne({ "_id":new ObjectId(data.id)},{$set:{'name':data.name, 'email':data.email, 'mobileNumber':data.mobileNumber,'age':data.age}});
+            console.log(isUpdated)
+            if (isUpdated.modifiedCount > 0 && isUpdated.acknowledged) {
+              callback({ status: true, msg: "User Updated" });
+            } else if(isUpdated.modifiedCount >= 0) {
+              callback({ status: false, msg: "User not updated" });
+            }else{
+              callback({ status: false, msg: "Failed to Update User" });
+            }
+          } catch (error) {
+            callback({ status: false,error:error.message});
+          }
+    }
 }
 
 module.exports = new userController();
--- routes/users.js
+++ routes/users.js
@@ -15,52 +15,25 @@ router.get("/getUser", async (req, res) => {
 //  Add Users
 router.post("/addUser", async (req, res) => {
   let newUser = req.body;
-  try {
-    let isAdded = await connection.collection("Users").insertOne(newUser);
-    if (isAdded) {
-      res.status(200).json({ status: true, msg: "New User Added" });
-    } else {
-      res.status(200).json({ status: false, msg: "Failed to Add New User" });
-    }
-  } catch (error) {
-    res.status(200).json({ status: false, error: error.message });
-  }
+  userController.addUser(newUser,(result)=>{
+    res.status(200).json(result)
+  })
 });
 
 // Delete Users
 router.delete("/deleteUser/:id", async (req, res) => {
   let id = req.params.id;
-  try {
-    let isDeleted = await connection
-      .collection("Users")
-      .deleteOne({ _id: new ObjectId(id) });
-    if (isDeleted) {
-      res.status(200).json({ status: true, msg: "User Deleted" });
-    } else {
-      res.status(200).json({ status: false, msg: "Failed to Delete User" });
-    }
-  } catch (error) {
-    res.status(200).json({ status: false, error: error.message });
-  }
+  userController.deleteUser(id,(result)=>{
+    res.status(200).json(result)
+  })
 });
 
 // Update Users
 router.put("/editUser", async (req, res) => {
   let data = req.body;
-  console.log(data.id)
-  try {
-    isUpdated = await connection.collection("Users").updateOne({ "_id":new ObjectId(data.id)},{$set:{'name':data.name, 'email':data.email, 'mobileNumber':data.mobileNumber,'age':data.age}});
-    console.log(isUpdated)
-    if (isUpdated.modifiedCount > 0 && isUpdated.acknowledged) {
-      res.status(200).json({ status: true, msg: "User Updated" });
-    } else if(isUpdated.modifiedCount >= 0) {
-      res.status(200).json({ status: false, msg: "User not updated" });
-    }else{
-      res.status(200).json({ status: false, msg: "Failed to Update User" });
-    }
-  } catch (error) {
-    res.status(200).json({ status: false,error:error.message});
-  }
+  userController.editUser(data,(result)=>{
+    res.status(200).json(result)
+  })
 });
 
 module.exports = router;
